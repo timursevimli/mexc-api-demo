5e248d5da44ef18338978086cd14e1756fe9f30c
svmlitimur <svmlitimur@gmail.com>
2024-03-21 00:44:58 +0300

    Create webSocketapi

--- websocket/ws.js
+++ websocket/ws.js
@@ -1,83 +1,205 @@
-const moment = require('moment');
+'use strict';
+
 const WebSocket = require('ws');
-const pako = require('pako');
+
 const WS_URL = 'wss://wbs.mexc.com/ws';
 
+const RECONN_INTERVAL = 5000;
+const PING_INTERVAL = 20_000;
 
-var ws = new WebSocket(WS_URL);
+// https://www.rfc-editor.org/rfc/rfc6455.html#section-7.4.1
+const NORMAL_CLOSE_CODE = 1000;
 
-ws.onopen = () => {
-    console.log('Connection open');
-    ws.send('{"method":"PING"}')
-    Deals()
-}
+const createConnection = () =>
+  new Promise((resolve, reject) => {
+    const socket = new WebSocket(WS_URL);
+    socket.once('error', reject);
+    socket.once('open', () => {
+      const isOpened = socket.readyState === WebSocket.OPEN;
+      if (isOpened) return void resolve(socket);
+      const message = `Connection server error, state: ${socket.readyState}`;
+      reject(new Error(message));
+    });
+  });
 
-ws.onmessage = e => {
-    console.log(e.data);
+const send = (socket, param) => {
+  const data = {
+    'method': 'SUBSCRIPTION',
+    'params': [param],
+  };
+  socket.send(JSON.stringify(data));
 };
 
-ws.onclose = () => {
-    console.log('close');
-}
+const webSocketApi = async () => {
+  let socket = await createConnection();
+
+  const listeners = [];
+  const params = [];
+  let messageCount = 0;
+
+  const reconnInit = () => {
+    for (const param of params) send(socket, param);
+    socket.on('message', (chunk) => {
+      messageCount++;
+      const data = JSON.parse(chunk.toString('utf8'));
+      for (const listener of listeners) listener(data);
+    });
+  };
+
+  let reconnInterval = setInterval(() => {
+    socket.close(NORMAL_CLOSE_CODE);
+    createConnection().then((connection) => {
+      socket = connection;
+      reconnInit();
+      console.log('Socket reconnected and listeners binded');
+    });
+  }, RECONN_INTERVAL);
+
+  let pingInterval = setInterval(() => {
+    socket.send('{"method":"PING"}');
+  }, PING_INTERVAL);
+
+  setTimeout(() => {
+    if (listeners.length === 0 && params.length === 0) {
+      clearInterval(pingInterval);
+      clearInterval(reconnInterval);
+      socket.close(NORMAL_CLOSE_CODE);
+      stream.end();
+    }
+  });
+
+  reconnInit();
+
+  return {
+    deals(symbols = 'BTCUSDT', cb = null) {
+      const param = `spot@public.deals.v3.api@${symbols}`;
+      const listener = (data) => void cb(data);
+      listeners.push(listener);
+      params.push(param);
+      send(socket, param);
+    },
+    kline(symbols, min, cb) {
+      const param = `spot@public.kline.v4.api@${symbols}@Min${min}`;
+      const listener = (data) => void cb(data);
+      listeners.push(listener);
+      params.push(param);
+      send(socket, param);
+    },
+    increaseDepth(symbols, cb) {
+      const param = `spot@public.increase.depth.v3.api@${symbols}`;
+      const listener = (data) => void cb(data);
+      listeners.push(listener);
+      params.push(param);
+      send(socket, param);
+    },
+    limitDepth(symbols, depth, cb) {
+      const param = `spot@public.limit.depth.v3.api@${symbols}@${depth}`;
+      const listener = (data) => void cb(data);
+      listeners.push(listener);
+      params.push(param);
+      send(socket, param);
+    },
+    bookTicker(symbols = 'BTCUSDT', cb = null) {
+      if (!cb) return;
+      const param = `spot@public.bookTicker.v3.api@${symbols}`;
+      const listener = (data = {}) => {
+        const { c, d, s } = data;
+        if (c === param) cb({ price: d.a, symbol: s });
+      };
+      listeners.push(listener);
+      params.push(param);
+      send(socket, param);
+    },
+    close(cb = null) {
+      if (cb) socket.on('close', cb);
+      if (pingInterval) {
+        clearInterval(pingInterval);
+        pingInterval = null;
+      }
+      if (reconnInterval) {
+        clearInterval(reconnInterval);
+        reconnInterval = null;
+      }
+      socket.close(NORMAL_CLOSE_CODE);
+      stream.end();
+    }
+  };
+};
+
+module.exports = webSocketApi;
+
+// ws.onopen = () => {
+//   console.log('Connection open');
+//   ws.send('{"method":"PING"}');
+//   Deals();
+// };
+//
+// ws.onmessage = (e) => {
+//   console.log(e.data);
+// };
+//
+// ws.onclose = () => {
+//   console.log('close');
+// };
 
 /**
  * deals
  * @param ws
  */
 function Deals() {
-    var data = {       
-        "method": "SUBSCRIPTION",
-        "params": ["spot@public.deals.v3.api@BTCUSDT"]       
-    }
-    ws.send(JSON.stringify(data));
+  const data = {
+    'method': 'SUBSCRIPTION',
+    'params': ['spot@public.deals.v3.api@BTCUSDT']
+  };
+  ws.send(JSON.stringify(data));
 }
 
 /**
  * kline
  * @param ws
  */
- function Kline() {
-    var data = {       
-        "method": "SUBSCRIPTION",
-        "params": ["spot@public.kline.v3.api@BTCUSDT@Min15"]       
-    }
-    ws.send(JSON.stringify(data));
+function Kline() {
+  const data = {
+    'method': 'SUBSCRIPTION',
+    'params': ['spot@public.kline.v4.api@BTCUSDT@Min15']
+  };
+  ws.send(JSON.stringify(data));
 }
 
 /**
  * increasedepth
  * @param ws
  */
- function IncreaseDepth() {
-    var data = {       
-        "method": "SUBSCRIPTION",
-        "params": ["spot@public.increase.depth.v3.api@BTCUSDT"]       
-    }
-    ws.send(JSON.stringify(data));
+function IncreaseDepth() {
+  const data = {
+    'method': 'SUBSCRIPTION',
+    'params': ['spot@public.increase.depth.v3.api@BTCUSDT']
+  };
+  ws.send(JSON.stringify(data));
 }
 
 /**
  * limitdepth
  * @param ws
  */
- function LimitDepth() {
-    var data = {       
-        "method": "SUBSCRIPTION",
-        "params": ["spot@public.limit.depth.v3.api@BTCUSDT@5"]       
-    }
-    ws.send(JSON.stringify(data));
+function LimitDepth() {
+  const data = {
+    'method': 'SUBSCRIPTION',
+    'params': ['spot@public.limit.depth.v3.api@BTCUSDT@5']
+  };
+  ws.send(JSON.stringify(data));
 }
 
 /**
  * bookTicker
  * @param ws
  */
- function BookTicker() {
-    var data = {       
-        "method": "SUBSCRIPTION",
-        "params": ["spot@public.bookTicker.v3.api@BTCUSDT"]       
-    }
-    ws.send(JSON.stringify(data));
+function BookTicker() {
+  const data = {
+    'method': 'SUBSCRIPTION',
+    'params': ['spot@public.bookTicker.v3.api@BTCUSDT']
+  };
+  ws.send(JSON.stringify(data));
 }
 
 /**
@@ -85,12 +207,12 @@ function Deals() {
  * WS_URL = 'wss://wbs.mexc.com/ws?listenKey=Your listenkey'
  * @param ws
  */
- function Account() {
-    var data = {       
-        "method": "SUBSCRIPTION",
-        "params": ["spot@private.account.v3.api"]       
-    }
-    ws.send(JSON.stringify(data));
+function Account() {
+  const data = {
+    'method': 'SUBSCRIPTION',
+    'params': ['spot@private.account.v3.api']
+  };
+  ws.send(JSON.stringify(data));
 }
 
 /**
@@ -98,12 +220,12 @@ function Deals() {
  * WS_URL = 'wss://wbs.mexc.com/ws?listenKey=Your listenkey'
  * @param ws
  */
- function AccountDeals() {
-    var data = {       
-        "method": "SUBSCRIPTION",
-        "params": ["spot@private.deals.v3.api"]       
-    }
-    ws.send(JSON.stringify(data));
+function AccountDeals() {
+  const data = {
+    'method': 'SUBSCRIPTION',
+    'params': ['spot@private.deals.v3.api']
+  };
+  ws.send(JSON.stringify(data));
 }
 
 /**
@@ -111,20 +233,10 @@ function Deals() {
  * WS_URL = 'wss://wbs.mexc.com/ws?listenKey=Your listenkey'
  * @param ws
  */
- function Orders() {
-    var data = {       
-        "method": "SUBSCRIPTION",
-        "params": ["spot@private.orders.v3.api"]       
-    }
-    ws.send(JSON.stringify(data));
+function Orders() {
+  const data = {
+    'method': 'SUBSCRIPTION',
+    'params': ['spot@private.orders.v3.api']
+  };
+  ws.send(JSON.stringify(data));
 }
-
-
-
-
-
-
-
-
-
-
